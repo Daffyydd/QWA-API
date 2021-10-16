bff13e84254ff21c7d7428a258f57373bf02c8a8
Daffyydd <dnyakundi@gmail.com>
2021-10-16 22:58:12 +0300

    3 files modified, 4 added

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -1,8 +1,6 @@
-import { HttpRequest } from "@angular/common/http";
-import { HttpHandler } from "@angular/common/http";
-import { HttpEvent } from "@angular/common/http";
-import { HttpInterceptor } from "@angular/common/http";
+import { HttpRequest,HttpHandler, HttpEvent,HttpInterceptor} from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { AuthenticationService } from "../_services";
 @Injectable()
